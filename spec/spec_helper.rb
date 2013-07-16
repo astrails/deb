237935cf5310f1e16e4ac1b46588d00b654b06f5
Boris Nadion <boris@astrails.com>
2013-07-16 19:36:36 +0300

    auto migrate test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-#require Rails.root.join('db/schema').to_s
+require Rails.root.join('db/schema').to_s unless File.exists?(Rails.root.join("db/test.sqlite3"))
 
 
 $: << File.expand_path(File.dirname(__FILE__) + '/../lib/')
